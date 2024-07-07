8de840701edd541f045717432a5473cb04e161f0
teacherIan <iansmalloy@gmail.com>
2024-07-06 21:44:31 -0400

    updates...

--- components/Lights.jsx
+++ components/Lights.jsx
@@ -1,36 +1,17 @@
 export default function Lights() {
+  const lightIntensity = 0;
   return (
     <>
-      <ambientLight intensity={3} />
-      <pointLight position={[5, 10, 5]} intensity={100} castShadow />
-      <pointLight position={[-5, 10, -5]} intensity={100} castShadow />
-      <pointLight position={[0, 10, 0]} intensity={100} castShadow />
-      <pointLight position={[5, 1, 5]} intensity={100} castShadow />
-      <pointLight position={[0, 1, 5]} intensity={100} castShadow />
+      <ambientLight intensity={9} />
+      <pointLight position={[5, 10, 5]} intensity={lightIntensity} castShadow />
       <pointLight
-        position={[3, 1, 1]}
-        intensity={100}
+        position={[-5, 10, -5]}
+        intensity={lightIntensity}
         castShadow
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
-        shadow-camera-far={100}
-        shadow-camera-left={-10}
-        shadow-camera-right={10}
-        shadow-camera-top={10}
-        shadow-camera-bottom={-10}
       />
-      <pointLight
-        position={[-5, 5, 5]}
-        intensity={100}
-        castShadow
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
-        shadow-camera-far={100}
-        shadow-camera-left={-10}
-        shadow-camera-right={10}
-        shadow-camera-top={10}
-        shadow-camera-bottom={-10}
-      />
+      <pointLight position={[0, 10, 0]} intensity={lightIntensity} castShadow />
+      <pointLight position={[5, 1, 5]} intensity={lightIntensity} castShadow />
+      <pointLight position={[0, 1, 5]} intensity={lightIntensity} castShadow />
     </>
   );
 }
