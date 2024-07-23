aa6deca9a1ef0f99e4e534eb865feb533c8d9cff
teacherIan <iansmalloy@gmail.com>
2024-07-22 22:06:34 -0400

    updates...

--- components/experience/Lights.jsx
+++ components/experience/Lights.jsx
@@ -2,16 +2,28 @@ export default function Lights() {
   const lightIntensity = 50;
   return (
     <>
-      <ambientLight intensity={5} />
-      <pointLight position={[5, 10, 5]} intensity={lightIntensity} castShadow />
+      <ambientLight intensity={3} />
       <pointLight
-        position={[-5, 10, -5]}
+        position={[-5, 10, 20]}
         intensity={lightIntensity}
         castShadow
       />
-      <pointLight position={[0, 10, 0]} intensity={lightIntensity} castShadow />
-      <pointLight position={[5, 20, 5]} intensity={lightIntensity} castShadow />
-      <pointLight position={[0, 20, 5]} intensity={lightIntensity} castShadow />
+      <pointLight
+        position={[-20, 10, -5]}
+        intensity={lightIntensity}
+        castShadow
+      />
+      <pointLight
+        position={[20, 10, -5]}
+        intensity={lightIntensity}
+        castShadow
+      />
+      <pointLight
+        position={[5, 20, 15]}
+        intensity={lightIntensity}
+        castShadow
+      />
+      <pointLight position={[2, 20, 2]} intensity={lightIntensity} castShadow />
     </>
   );
 }
