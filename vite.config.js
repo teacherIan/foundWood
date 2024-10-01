6c4d8789c1fd5f7021543c9538d00ca56058b15c
teacherIan <iansmalloy@gmail.com>
2024-10-01 05:15:09 -0400

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path'; // add import for path
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +9,12 @@ export default defineConfig({
   server: {
     host: true,
   },
+  // resolve: {
+  //   alias: [
+  //     {
+  //       find: '../font',
+  //       replacement: path.resolve(__dirname, '/assets/fonts'),
+  //     },
+  //   ],
+  // },
 });
