f280db13a7eb006a8590039cbe9486214b98fca7
teacherIan <iansmalloy@gmail.com>
2025-04-30 11:20:56 +0800

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import NewApp from './NewApp';
+import { Analytics } from '@vercel/analytics/react';
 
 document.addEventListener(
   'touchmove',
@@ -18,5 +19,6 @@ window.addEventListener('resize', () => {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NewApp />
+    <Analytics />
   </React.StrictMode>
 );
