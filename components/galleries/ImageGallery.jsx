b4c53bc4d00da5af190aaac14318da27fcb45238
Ian Malloy <iansmalloy@gmail.com>
2025-08-24 20:19:25 -0400

    updates..

--- components/galleries/ImageGallery.jsx
+++ components/galleries/ImageGallery.jsx
@@ -174,23 +174,6 @@ const ImageGalleryComponent = ({
           onImageError={() => console.log('Image error')}
         />
       </div>
-      
-      {/* Close button moved to corner */}
-      <button 
-        className={styles.closeButton}
-        onClick={onClose}
-        aria-label="Close Gallery"
-        style={{
-          position: 'absolute',
-          top: '1rem',
-          right: '1rem',
-          zIndex: 200
-        }}
-      >
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-          <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
-        </svg>
-      </button>
     </div>
   );
 };
